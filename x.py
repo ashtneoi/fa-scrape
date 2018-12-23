d264b0b648d4bb967b71c1c60a913fedc1b94611
ashtneoi <ashtneoi@gmail.com>
2018-12-22 17:35:40 -0800

    Get submission URLs from first page

--- x.py
+++ x.py
@@ -7,7 +7,20 @@ from sys import stderr
 import activate
 activate.activate()
 
+import requests
 from bs4 import BeautifulSoup
+
+
+def get_submission_urls(gallery_url):
+    r = requests.get(gallery_url)
+    if not r.ok:
+        stderr.write(r.text)
+        r.raise_for_status()
+
+    doc = BeautifulSoup(r.content, 'html.parser')
+    gallery = doc.find('section', id='gallery-gallery')
+    for caption in gallery.find_all('figcaption'):
+        yield caption.a['href']
 
 
 def main(argv):
@@ -15,6 +28,9 @@ def main(argv):
     p.add_argument('URL')
     p.add_argument('DIR')
     a = p.parse_args(argv[1:])
+
+    for url in get_submission_urls(a.URL):
+        print(url)
 
 
 if __name__ == '__main__':
