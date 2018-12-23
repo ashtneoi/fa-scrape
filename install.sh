2efda8caf7bb2430bc582240535b870fc7b09620
ashtneoi <ashtneoi@gmail.com>
2018-12-22 16:40:44 -0800

    Ignore files and add virtualenv installer

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+shopt -s nullglob
+
+if ! [[ -d .env ]]; then
+    virtualenv .env -p $(which python3)
+fi
+
+source .env/bin/activate
+
+pip3 install beautifulsoup4==4.6.3
