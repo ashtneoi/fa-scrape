3ba59e6147fbc0bd07183455b586379cdf6cceda
ashtneoi <ashtneoi@gmail.com>
2018-12-22 19:15:43 -0800

    Get all the important stuff

--- x.py
+++ x.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import urllib
+from os import path
 from sys import stderr
 from time import sleep
 
@@ -19,6 +20,12 @@ def ltake(s, t):
     return s[len(t):]
 
 
+def rtake(s, t):
+    if not s.endswith(t):
+        raise ValueError("unexpected suffix")
+    return s[:-len(t)]
+
+
 def get_submission_urls(gallery_url):
     r = requests.get(gallery_url)
     if not r.ok:
@@ -29,11 +36,12 @@ def get_submission_urls(gallery_url):
     gallery = doc.find('section', id='gallery-gallery')
     for caption in gallery.find_all('figcaption'):
         href = caption.a['href']
+        submission_id = rtake(ltake(href, '/view/'), '/')
         url = urllib.parse.urljoin(
             gallery_url,
-            '/full/' + ltake(href, '/view/'),
+            f'/full/{submission_id}/',
         )
-        yield url
+        yield submission_id, url
 
 
 def get_submission_info(submission_url):
@@ -51,7 +59,7 @@ def get_submission_info(submission_url):
 
     title = doc.find('meta', property='og:title')['content']
 
-    return title, image_url
+    return r.content, title, image_url
 
 
 def main(argv):
@@ -60,9 +68,38 @@ def main(argv):
     p.add_argument('DIR')
     a = p.parse_args(argv[1:])
 
-    for s in get_submission_urls(a.URL):
-        print(get_submission_info(s))
-        sleep(5)
+    for submission_id, submission_url in get_submission_urls(a.URL):
+        page_content, title, image_url = get_submission_info(submission_url)
+
+        r = requests.get(image_url, stream=True)
+        if not r.ok:
+            stderr.write(r.text)
+            r.raise_for_status()
+
+        assert '.' in image_url
+        image_out_path = path.join(
+            a.DIR,
+            '{} {}.{}'.format(
+                submission_id,
+                title,
+                image_url[image_url.rfind('.')+1:],
+            ),
+        )
+        print(image_out_path)
+
+        with open(image_out_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1<<14):
+                f.write(chunk)
+
+        page_out_path = path.join(
+            a.DIR,
+            f'{submission_id} {title}.html',
+        )
+
+        with open(page_out_path, 'wb') as f:
+            f.write(page_content)
+
+        sleep(3)
 
 
 if __name__ == '__main__':
